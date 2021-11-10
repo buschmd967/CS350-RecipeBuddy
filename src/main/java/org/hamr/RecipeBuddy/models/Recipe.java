efefaf194cbef3b5c141c92193d121834dc8d232
buschmd967 <buschmd@clarkson.edu>
2021-11-10 10:52:43 -0500

    Added Bare-bones recipe and recipe Repository

---
+++ src/main/java/org/hamr/RecipeBuddy/models/Recipe.java
@@ -0,0 +1,113 @@
+package org.hamr.RecipeBuddy.models;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "recipes")
+public class Recipe {
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max=80)
+    private String name;
+
+    @NotBlank
+    @Size(max=20)
+    private String author;
+
+    private String[] dietaryRestrictions;
+
+    private String[] appliances;
+
+    private String[] otherTags;
+
+    //private Comment[] comments
+
+    //private Step[] steps
+
+    private short rating;
+
+    private short difficulty;
+
+    private String image;
+
+    private short servings;
+
+    //Time to cook in seconds
+    private int cookTime;
+
+    public Recipe(String name, String author){
+        this.name = name;
+        this.author = author;
+    }
+
+    public void setDietaryRestrictions(String[] dietaryRestrictions){
+        this.dietaryRestrictions = dietaryRestrictions;
+    }
+
+    public String[] getDietaryRestrictions(){
+        return dietaryRestrictions;
+    }
+
+    public void setAppliances(String[] appliances){
+        this.appliances = appliances;
+    }
+
+    public String[] getAppliances(){
+        return appliances;
+    }
+
+    public void setOtherTags(String[] otherTags){
+        this.otherTags = otherTags;
+    }
+
+    public String[] getOtherTags(){
+        return otherTags;
+    }
+
+    public void setRating(short rating){
+        this.rating = rating;
+    }
+
+    public short getRating(){
+        return rating;
+    }
+
+    public void setDifficulty(short difficulty){
+        this.difficulty = difficulty;
+    }
+
+    public short getDifficulty(){
+        return difficulty;
+    }
+
+    public void setImage(String image){
+        this.image = image;
+    }
+
+    public String getImage(){
+        return image;
+    }
+
+    public void setServings(short servings){
+        this.servings = servings;
+    }
+    
+    public short getServings(){
+        return servings;
+    }
+
+    public void setCookTime(int cookTime){
+        this.cookTime = cookTime;
+    }
+
+    public int getCookTime(){
+        return cookTime;
+    }
+}
---
+++ src/main/java/org/hamr/RecipeBuddy/repository/RecipeRepository.java
@@ -0,0 +1,10 @@
+package org.hamr.RecipeBuddy.repository;
+
+import java.util.Optional;
+
+import org.hamr.RecipeBuddy.models.Recipe;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RecipeRepository extends MongoRepository<Recipe, String>{
+    Optional<Recipe> findByName(String name);
+}
