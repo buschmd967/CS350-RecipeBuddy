dc662b5a95d8a861dc0f4e68d54d0ed33223287a
Morgan Busch <buschmd@clarkson.edu>
2021-12-05 20:01:00 -0500

    very minor homepage stuff

---
+++ src/main/resources/public/scripts/homepage.js
@@ -0,0 +1,26 @@
+var recipeTemplate = `
+<div class="recipeInfo">
+                    
+<h2>NAME</h2>
+<div id="recipeText">
+    <h3>Author: </h3>
+    <h4>Rating: </h4>
+    <h4>Ingredients: (Listed as a paragraph)</h4>
+    <h4>Dietary Restricitions: (Listed as a paragraph)</h4>
+</div>
+
+<div class="recipeTable">
+    <table id="toClear">
+        <tr class="recipeData">
+            <image height=200 width=200 src="">
+        </tr>
+    </table>
+</div>
+</div>`
+
+
+$(document).ready(function() {
+
+    
+    }
+);
