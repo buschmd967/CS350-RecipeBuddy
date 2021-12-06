15188f823e92f733a48741c16d611f02b7a9999e
delwicrg <delwicrg@clarkson.edu>
2021-12-06 16:13:59 -0500

    name and author to cook recipe page

--- src/main/resources/public/scripts/cookRecipe.js
+++ src/main/resources/public/scripts/cookRecipe.js
@@ -35,6 +35,17 @@ $(document).ready(function() {
         recipe = data;
         console.log(data);
 
+        $("#name").append(recipe["name"]);
+        $("#author").append(recipe["author"]);
+
+        let cookTime = recipe["cookTime"];
+        if(cookTime == 0){
+            $("#cookTime").append("No cook time specified.");
+        }
+        else{
+            $("#cookTime").append(`Approximate Cook Time: ${timerDisplay(cookTime)}`); 
+        }
+
         /*
         $("#name").append(recipe["name"]);
         $("#author").append(recipe["author"]);
