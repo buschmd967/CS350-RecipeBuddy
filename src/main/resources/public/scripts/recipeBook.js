588ced4444dc8be786e7e0f5bccb1b56d4170370
delwicrg <delwicrg@clarkson.edu>
2021-12-14 16:50:46 -0500

    cookRecipe logout redirect

--- src/main/resources/public/scripts/recipeBook.js
+++ src/main/resources/public/scripts/recipeBook.js
@@ -1,66 +1,39 @@
-/* Rosie's Disclaimer: I have not gotten much, this is basically just code copy
-and pasted for searchRecipe. 
-
-My intial thought was that I could essentially go off
-the search page but instead of being able to search by any string,
-the string is automatically the username. In order for this to happen, I added the 
-authors user name to othertags, allowing search by usernmae to be accessible. 
-
-I remeber sean saying that he rather have a couple working pages than multiple half working 
-so I have switched over to testing and adding recipes for our demo
-
-I plan to revisit this at a later time, as I would like to figure it out.
-
-*/
-
-
-
+var username = "";
 $(document).ready(function() {
-    
-
-    if($.cookie("jwt") === undefined){ //if guest
-        $("#MyProfile").html("Login");
-        $("#MyProfile").attr("href", "login?redir=searchRecipe");
-    }
 
     let c = $.cookie("jwt");
-    let username = "";
-    getUsername(c).then(data =>{
-        username = data["message"];
-        console.log(username);
-    });
 
-    let params = new URLSearchParams(window.location.search);
-    let searchString = username;
+    if(c === undefined){
+        document.location="/homepage";
+    }
+    else {
+        getUsername(c).then(data =>{
+            username = data["message"];
+        });
+    }
+
     console.log(username);
+
+    let searchString = username;
     console.log(searchString);
 
-   
     search();
 
 });
 
-function getUsername(c){
-    return $.ajax({
-            url: 'http://localhost:8080/api/user/username',
-            type: 'post',
-            headers: {"Authorization": "Bearer " + $.cookie("jwt")},
-            xhrFields: { withCredentials:true },
-            contentType: 'application/json',
-            success: function(response){
-                console.log("GOT USERNAME");
-            },
-            complete: function(xhr, textStatus) {
-                console.log(xhr.status);
-            } 
-        });
-}
-
 
 function search(limit=-1){
-    let searchString = getSearchString(limit);
-    $("#results").children("#toClear").remove();
 
+    // problem is that i dont get the username until after it searches
+    let jwt = $.cookie("jwt");
+
+    getUsername(jwt).then(data =>{
+        username = data["message"];
+    });
+
+    let searchString = username; // theoretically if this gets the write username then it will return results
+
+    $("#results").children("#toClear").remove();
 
     $.ajax({
         url: 'http://localhost:8080/api/recipe/search',
@@ -80,74 +53,43 @@ function search(limit=-1){
             if(xhr.status != 200){
                 console.log(xhr)
                 if(xhr.status == 400){
-                    $("#status").text("Bad Request. Please make sure all required fields have been filled in.");
+                    $("#status").text("");
                     
                 }
                 if(xhr.status == 401){
-                    document.location="/login?redir=searchRecipe";
+                    document.location="/login?redir=recipeBook";
                 }
             }
         } 
     }).then(function(data){
-        if(data["error"] && data["message"] == "Could not find any matching recipies"){
-            if(limit == -1){
-                limit = searchString.split(",").length - 1;
-                if(limit > 0){
-                    search(limit);
-                }
-                else{ /* modified*/
-                    $("#status").text("Could not find any recipes in your Recipe Book");
-                }
-            }
-            else if(limit == 1){
-                $("#status").text("Could not find any recipes in your Recipe Book");
-            }
-            else{
-                search(limit-1);
-            }
-        }
-        else{
             console.log("Data:");
             console.log(data);
             displayRecipies(data);
             if(limit != -1){
-                $("#status").html("Initial search did not yield results. The following are results for: <br>" + searchString);
+                $("#status").html("Your RecipeBook is empty");
             }
             else{
                 $("#status").text(data.message);
             }
-        }
-        
+            
     });
 }
 
 
-function getSearchString(limit){
-    let c = $.cookie("jwt");
-    let username = "";
-    getUsername(c).then(data =>{
-    username = data["message"];
-    console.log("got username");
-    console.log(username);
-
-    });
-
-    if(limit == -1)
-        return username;
-    else{
-        let searchString = username;
-        let outputSearchString = "";
-        for(let i = 0; i < limit; i++){
-            if(i == limit - 1){
-                outputSearchString += searchString[i];
-            }
-            else{
-                outputSearchString += searchString[i] + ",";
-            }
-        }
-        return outputSearchString;
-
-    }
+function getUsername(c){
+    return $.ajax({
+            url: 'http://localhost:8080/api/user/username',
+            type: 'post',
+            headers: {"Authorization": "Bearer " + $.cookie("jwt")},
+            xhrFields: { withCredentials:true },
+            contentType: 'application/json',
+            success: function(response){
+                console.log("SUCCESS");
+            },
+            complete: function(xhr, textStatus) {
+                console.log(xhr.status);
+            } 
+        });
 }
 
 function displayRecipies(data){
@@ -162,10 +104,6 @@ function displayRecipies(data){
         return;
     }
     for(let recipe of recipies){
-        // console.log("in for loop");
-        // console.log(recipe);
-        // console.log(recipe["name"]);
-        // console.log(recipeEntry);
         let name = recipe["name"];
         let author = `${recipe["displayAuthor"]} (${recipe["author"]})`;
         let rating = Math.round(recipe["rating"] * 100) / 100;
@@ -194,7 +132,6 @@ function displayRecipies(data){
                 </div>
            `);
     }
-    // console.log(data["recipies"][0]);
 }
 
 function redirect(name, author){
